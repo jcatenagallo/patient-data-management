b11292c8bc51a9a304ea43743a3b0d453588df82
Juan Catena <jcatena@litebox.ai>
2024-08-04 02:02:12 -0300

    Create add new patient button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import tw from 'twin.macro';
+import { UserPlusIcon } from '@heroicons/react/24/outline';
 
 import Layout from '@/components/Layout';
 import useGetPatientsRecords from '@/hooks/api/useGetPatientsRecord';
@@ -15,6 +16,21 @@ tablet:grid-cols-2
 desktop:grid-cols-3
 `;
 
+const StyledAddPatientButton = tw.button`
+absolute
+bottom-4
+right-4
+flex
+gap-2
+items-center
+p-4
+rounded-full
+bg-greens-sage
+text-darks-black
+
+hover:bg-greens-viridian
+`;
+
 export default function Home() {
   const { data } = useGetPatientsRecords();
 
@@ -23,6 +39,10 @@ export default function Home() {
       <StyledWrapper>
         {data?.map((item) => <Card key={item.id} patientData={item} />)}
       </StyledWrapper>
+      <StyledAddPatientButton>
+        <UserPlusIcon className="h-6 w-6 text-darks-black" />
+        Add New Patient
+      </StyledAddPatientButton>
     </Layout>
   );
 }
